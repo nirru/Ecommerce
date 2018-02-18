e559ca4512435b19c1771a94e12deb76272f41e4
nirru <nikk3008@outlook.com>
2018-02-19 01:25:45 +0530

    Initial commit

---
+++ app/src/main/java/com/oxilo/ecommerce/network/api/WebService.java
@@ -0,0 +1,64 @@
+package com.oxilo.ecommerce.network.api;
+
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Response;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+
+/**
+ * Created by ericbasendra on 10/06/16.
+ */
+public interface WebService {
+
+    @GET("ipif/index.php/api/get_categories_list")
+    io.reactivex.Observable<Response<ResponseBody>> getCategory();
+
+    @FormUrlEncoded
+    @POST("ipif/index.php/api/login")
+    io.reactivex.Observable<Response<ResponseBody>> login(
+            @Field("phone_num") String phone,
+            @Field("device_token") String device_token);
+
+    @GET("ipif/index.php/api/get_brands_list")
+    io.reactivex.Observable<Response<ResponseBody>> getAllProduct(
+            @Query("tag") String popular);
+
+    @GET("ipif/index.php/api/get_brands_list")
+    io.reactivex.Observable<Response<ResponseBody>> getBrand(
+            @Query("tag") String brand);
+
+    @GET("ipif/index.php/api/get_brands_list")
+    io.reactivex.Observable<Response<ResponseBody>> getBrandsByLocations(
+            @Query("tag") String location,
+            @Query("latitude") String latitude,
+            @Query("longitude") String longitude,
+            @Query("miles") String miles);
+
+    @GET("ipif/index.php/api/get_categories_by_brand_id")
+    io.reactivex.Observable<Response<ResponseBody>> getCategoriesByBrandId(
+            @Query("brand_id") String brand_id);
+
+    @GET("ipif/index.php/api/get_products_list")
+    io.reactivex.Observable<Response<ResponseBody>> get_products_list(
+            @Query("category_id") String category_id);
+
+    @GET("ipif/index.php/api/get_products_detail")
+    io.reactivex.Observable<Response<ResponseBody>> get_products_detail(
+            @Query("product_id") String product_id);
+
+    @Headers("Accept: multipart/form-data")
+    @Multipart
+    @POST("ipif/index.php/api/userprofile")
+    io.reactivex.Observable<Response<ResponseBody>> uploadProfile(@Part MultipartBody.Part image,
+                                                                  @Part("name") RequestBody name,
+                                                                  @Part("user_id") RequestBody user_id);
+}
---
+++ app/src/main/java/com/oxilo/ecommerce/view/activity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.oxilo.ecommerce.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.oxilo.ecommerce.R;
+import com.oxilo.ecommerce.view.BottomNavigationViewHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_acount:
+                    mTextMessage.setText(R.string.title_account);
+                    return true;
+                case R.id.navigation_more:
+                    mTextMessage.setText(R.string.title_more);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
